80023bb9c167219349df8b0fba22eae9889a89ed
Marcelo Iepsen <iepsen@gmail.com>
2021-05-26 20:19:35 +0200

    Add version required formula field

--- cmvm.rb
+++ cmvm.rb
@@ -5,6 +5,7 @@ class Cmvm < Formula
   homepage "https://github.com/iepsen/cmvm/"
   url "https://raw.githubusercontent.com/iepsen/cmvm/master/cmvm"
   sha256 "be0cb38432b567b801bacf0a3076f5d9bb1e64897e445575e4ac40b9dc56aa58"
+  version "0.1.0"
 
   def install
     cmvm_path = ENV["HOME"] + "/.cmvm"
