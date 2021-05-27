df9167d6649814dc2b23833311676870174adbdc
Marcelo Iepsen <iepsen@gmail.com>
2021-05-27 19:07:52 +0200

    Fix cmvm url

--- cmvm.rb
+++ cmvm.rb
@@ -1,7 +1,7 @@
 class Cmvm < Formula
   desc "Manage multiple versions of CMake"
     homepage "https://github.com/iepsen/cmvm/"
-    url "https://raw.githubusercontent.com/iepsen/cmvm/cmvm"
+    url "https://raw.githubusercontent.com/iepsen/cmvm/master/cmvm"
     sha256 "45bf7efb0d8ba6c0ae19787f4e97b0ea89d52f91cf60b559c1ef619bf6639459"
     version "0.2.1"
     bottle :unneeded
