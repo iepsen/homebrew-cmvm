34b1ec968cd92e56b5bb8898dfbfc1259684dbde
Marcelo Iepsen <iepsen@gmail.com>
2021-05-27 19:02:54 +0200

    Update install code and version

--- cmvm.rb
+++ cmvm.rb
@@ -1,17 +1,17 @@
 class Cmvm < Formula
-  desc "CMake Version Manager brew formula"
-  homepage "https://github.com/iepsen/cmvm/"
-  url "https://raw.githubusercontent.com/iepsen/cmvm/master/cmvm"
-  sha256 "be0cb38432b567b801bacf0a3076f5d9bb1e64897e445575e4ac40b9dc56aa58"
-  version "0.1.0"
-
-  def install
-    mkdir_p "#{ENV["HOME"]}/.cmvm/bin"
-    mkdir_p "#{ENV["HOME"]}/.cmvm/versions"
-    bin.install "cmvm"
+  desc "Manage multiple versions of CMake"
+    homepage "https://github.com/iepsen/cmvm/"
+    url "https://raw.githubusercontent.com/iepsen/cmvm/cmvm"
+    sha256 "45bf7efb0d8ba6c0ae19787f4e97b0ea89d52f91cf60b559c1ef619bf6639459"
+    version "0.2.1"
+    bottle :unneeded
+  
+    def install
+      bin.install "cmvm"
+    end
+  
+    test do
+      system "#{bin}/cmvm"
+    end
   end
-
-  test do
-    system "#{bin}/cmvm"
-  end
-end
+  
