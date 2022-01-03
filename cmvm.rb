c251eac5a913509c5eb5d0c76866502e2f39ca17
Marcelo Iepsen <iepsen@gmail.com>
2022-01-03 21:14:28 +0100

    Remove deprecated "bottle :unneeded"

--- cmvm.rb
+++ cmvm.rb
@@ -3,8 +3,7 @@ class Cmvm < Formula
     homepage "https://github.com/iepsen/cmvm/"
     url "https://github.com/iepsen/cmvm/archive/refs/tags/v0.2.2.zip"
     sha256 "040368625f24bcf5046ec6b10a1eaf2ea87506d747695b84fa6e5dfd5939c780"
-    version "0.2.2"
-    bottle :unneeded
+    version "0.2.3"
   
     def install
       bin.install "cmvm"
